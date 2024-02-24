3aa9e7236a4ce73680383e44ac6074eba0516e82
Atharva Kshirsagar <atharvaksh@gmail.com>
2024-02-24 04:09:12 -0500

    Added components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
-import logo from './logo.svg';
 import react from 'react';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Experience from './components/Experience';
+import Projects from './components/Projects';
+import Testimonials from './components/Testimonials';
+import Contact from './components/Contact';
+import Skills from './components/Skills';
+
+
 
 function App() {
   return (
-    <main>
+    <main className='text-gray-400 bg-gray-900 body-font'>
       <Navbar />
       <About />
       <Experience />
---
+++ frontend/src/components/About.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+function About() {
+    return (
+        <section id="about">
+            <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+                <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                    <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+                        <b>Hi, I'm Atharva Kshirsagar.</b>
+                        <br className="hidden lg:inline-block" />I love to build cool Web and Machine Learning applications.
+                    </h1>
+                    <p className="mb-8 leading-relaxed">
+                        I am currently working as a Software Engineer with a focus on Machine Learning and Backend Engineering.
+                        I love playing guitar, hiking and exploring new food joints near me.
+                    </p>
+                    <div className="flex justify-center">
+                        <a
+                            href="#projects"
+                            className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+                            See My Past Work
+                        </a>
+                        <a
+                            href="#contact"
+                            className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+                            Work With Me
+                        </a>
+                    </div>
+                </div>
+                <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+                    <img
+                        className="object-cover object-center rounded-lg w-full md:max-w-lg max-h-90 md:h-auto"
+                        alt="hero"
+                        src="/images/hero.jpg"
+                    />
+                </div>
+            </div>
+        </section>
+    )
+};
+
+export default About;
